817b07f801437b3c9b7aec54324ce9eed4a58bdc
Thibault Schowing <th.schow@gmail.com>
2019-09-18 13:30:38 +0200

    second exercice serie 2 first draw done

--- Day 2/ExercisesDay2_Graphics2019.R
+++ Day 2/ExercisesDay2_Graphics2019.R
@@ -2,7 +2,7 @@
 # R course 2019
 # Exercises day 2 - Graphics
 # -------------------------------------------------------------------
-
+rm(list = ls())
 # Exercise 2.9 --------------------------------------------------------------------
 #
 # Load the StatWiSo2003 data set using the function read.table().
@@ -15,6 +15,12 @@
 #    - A barplot with barplot() function?
 #       Use these different functions.
 #       What can you learn from the different plots?
+df <- read.table("./StatWiSo2003.txt", header = TRUE, sep = '\t', na.strings = "?")
+
+plot(df$RandomNumber)
+boxplot(df$RandomNumber)
+barplot(df$RandomNumber)
+hist(df$RandomNumber)
 
 
 # 2.9.2 Make two graphs of the random numbers ("RandomNumber" column),
@@ -27,11 +33,26 @@
 #       
 #       After plotting the data, is there a difference between males and females?
 
+layout(matrix(c(1:2),nrow=1)); layout.show(2)
+
+femalesIdx <- which(df$Gender == "W")
+malesIdx <- which(df$Gender == "M")
+
+plotFemale <- boxplot(df[femalesIdx,"RandomNumber"])
+plotMale <- boxplot(df[malesIdx,"RandomNumber"])
+
+
 
 # 2.9.3 Plot the weight (dependent variable) against 
 #       height (independent variable). What do you see from that plot? 
 #       Is the pattern the same if we consider males and females separatly?
 #Quick results
+
+plot(df$Weight, df$Height)
+
+plot(df[femalesIdx, "Weight"], df[femalesIdx, "Height"])
+plot(df[malesIdx, "Weight"], df[malesIdx, "Height"])
+
 
 
 # 2.9.5. Improve the graph from 2.9.3 by:
@@ -44,11 +65,26 @@
 #              Use that vector of color names (with the same size as the number of points) 
 #              as input for the argument "col=" of function plot()
 
+df_full <- na.omit(df)
+idf = which(df_full$Gender == "W")
+idm = which(df_full$Gender == "M")
+vec_colors <- c(rep("Blue", length(df_full$Gender)))
+vec_colors[idf] = "Red"
+
+dev.off()
+
+
+plot(df_full$Weight, df_full$Height, col=vec_colors)
+
 
 # 2.9.5 Make a plot that shows whether 50% of all students pay less or 
 #       more than 500 Fr monthly rent (MonMiete) 
 #       (excluding the ones that pay no rent)
 
+df_rent <- df[which(df$MonthlyRent > 0),]
+
+boxplot(df_rent$MonthlyRent)
+abline(h = 500, col = "red")
 
 # Exercise 2.10 -------------------------------------------------------------------
 # 
@@ -63,3 +99,8 @@
 # 2.10.4 Use the function rug() to add the data points.
 # 2.10.5 Repeat the same exercise, but with a sample of 20 values.
 
+
+# WTF with the y limit ?! (doesn't show anything cuz it's too low.)
+x <- rnorm(100, 170, 20)
+hist(x, ylab = "frequency", main = "Histogram of sample drawn from a Normal Distribution", xlim = c(100,230))
+rug(x)
